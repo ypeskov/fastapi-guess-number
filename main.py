8d2c7fdbcb2f23991fe8e2e26e9805e3992e6527
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-05-16 00:37:02 +0300

    fixed logic, added extra info in output

--- main.py
+++ main.py
@@ -14,25 +14,42 @@ MIN_NUMBER = 1
 MAX_NUMBER = 100
 INITIAL_ATTEMPT = 1
 number_of_attempts: int = INITIAL_ATTEMPT
-current_number: int = round(MAX_NUMBER / 2)
+current_number: int = round((MAX_NUMBER - MIN_NUMBER) / 2)
+prev_low: int = MIN_NUMBER
+prev_high: int = MAX_NUMBER
+
 
 @app.get('/', response_class=HTMLResponse)
 async def home(request: Request):
+    global current_number, number_of_attempts, prev_low, prev_high
+    number_of_attempts = INITIAL_ATTEMPT
+    current_number = round((MAX_NUMBER - MIN_NUMBER) / 2)
+    prev_low = MIN_NUMBER
+    prev_high = MAX_NUMBER
+
     context = { 'request': request}
     return templates.TemplateResponse('home.html', context=context)
 
 @app.get('/attempt', response_class=HTMLResponse)
 async def make_attempt_to_guess(request: Request, guessed: str = None):
-    global number_of_attempts, current_number
+    global number_of_attempts, current_number, prev_low, prev_high
     context = { 'request': request }
     
     if guessed == 'yes':
         tpl = 'victory.html'
+        context['number_of_attempts'] = number_of_attempts
     else:
         tpl = 'attempt_failed.html'
+        
         if number_of_attempts > INITIAL_ATTEMPT:
-            current_number = round(current_number / 2)
-        number_of_attempts += 1
+            if guessed == 'less':
+                prev_high = current_number
+                current_number = round((prev_high - prev_low) / 2) + prev_low
+            elif guessed == 'bigger':
+                prev_low = current_number
+                current_number = round(prev_high - (prev_high - current_number)/2 )
+    
+    number_of_attempts += 1
 
     context['number'] = current_number
 
