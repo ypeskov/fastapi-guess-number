1363da6e685bf11c88a941f4d2d7734ac9b3ecc4
Yuriy Peskov <yuriy.peskov@gmail.com>
2023-05-16 10:20:27 +0300

    refactoring and removing hardcode

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pprint import pprint
+from math import floor
 
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
@@ -14,23 +15,23 @@ MIN_NUMBER = 1
 MAX_NUMBER = 100
 INITIAL_ATTEMPT = 1
 number_of_attempts: int = INITIAL_ATTEMPT
-current_number: int = round((MAX_NUMBER - MIN_NUMBER) / 2)
+current_number: int = floor((MAX_NUMBER - MIN_NUMBER) / 2) + 1 
 prev_low: int = MIN_NUMBER
 prev_high: int = MAX_NUMBER
 
 
-@app.get('/', response_class=HTMLResponse)
+@app.get('/', response_class=HTMLResponse, name='home')
 async def home(request: Request):
     global current_number, number_of_attempts, prev_low, prev_high
     number_of_attempts = INITIAL_ATTEMPT
-    current_number = round((MAX_NUMBER - MIN_NUMBER) / 2)
+    current_number = floor((MAX_NUMBER - MIN_NUMBER) / 2) + 1
     prev_low = MIN_NUMBER
     prev_high = MAX_NUMBER
 
-    context = { 'request': request}
+    context = { 'request': request }
     return templates.TemplateResponse('home.html', context=context)
 
-@app.get('/attempt', response_class=HTMLResponse)
+@app.get('/attempt', response_class=HTMLResponse, name='attempt')
 async def make_attempt_to_guess(request: Request, guessed: str = None):
     global number_of_attempts, current_number, prev_low, prev_high
     context = { 'request': request }
@@ -44,10 +45,10 @@ async def make_attempt_to_guess(request: Request, guessed: str = None):
         if number_of_attempts > INITIAL_ATTEMPT:
             if guessed == 'less':
                 prev_high = current_number
-                current_number = round((prev_high - prev_low) / 2) + prev_low
+                current_number = floor((prev_high - prev_low) / 2) + 1 + prev_low
             elif guessed == 'bigger':
                 prev_low = current_number
-                current_number = round(prev_high - (prev_high - current_number)/2 )
+                current_number = floor(prev_high - (prev_high - current_number) / 2) + 1
     
     number_of_attempts += 1
 
